bb0a655b019b8b7cee91b6f701d0255208b5068d
Anton Sameishchev <anton.sameishchev@devexpress.com>
2021-10-30 14:09:53 +0300

    Code added

---
+++ CS/ExportGridControl/ExportGridControl/Views/GridPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.WinUI.Grid.Printing;
+using DevExpress.WinUI.Printing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace ExportGridControl {
+
+    public sealed partial class GridPage : Page {
+
+        public GridPage() {
+            InitializeComponent();
+        }
+
+        public ViewModel ViewModel { get; } = new ViewModel();
+
+        void Button_Click(object sender, RoutedEventArgs e) {
+            var documentSource = new PrintableLink(new GridControlPrinter(gridcontrol));
+            documentSource.CreateDocument();
+            documentSource.PrintingSystem.ExportToCsv(@"C:\Users\Public\Documents\Report.csv");
+        }
+    }
+}
